534cf77fa362008ebd9457de68c2b4da8961ca30
Rajath hegde <rajat.hegde@zibtek.com>
2023-02-15 20:49:00 +0530

    app.js reduce

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './Components/Header';
-import Middle from './Components/Middle';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import VideoPly from './Components/VideoPly';
-import { BrowserRouter , Routes, Route} from 'react-router-dom';
-import Home from './Components/Home';
-import ucrclogo from './Images/ucrc-logo.png'
-import Routing from './Components/Routing';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Routing from "./Components/Routing";
 
 function App() {
-  return (
-    <BrowserRouter>
-        
-       <Routing/>
-      </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routing />
+        </BrowserRouter>
+    );
 }
 
 export default App;
