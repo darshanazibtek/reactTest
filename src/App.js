d826a71b8ed04432a4a89fec94cb700c297ddcab
darshana <rajat.hegde@zibtek.com>
2023-02-15 20:54:37 +0530

    changed to arrow function

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Routing from "./Components/Routing";
 
-function App() {
+let App = () => {
     return (
         <BrowserRouter>
             <Routing />
         </BrowserRouter>
     );
-}
+};
 
 export default App;
